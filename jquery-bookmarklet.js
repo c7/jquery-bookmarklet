a63d9453ad2ee0d958be45c309b9d6c1be059459
Peter Hellberg <peter@c7.se>
2008-12-12 00:53:05 +0100

    Added console message

--- jquery-bookmarklet.js
+++ jquery-bookmarklet.js
@@ -1,12 +1,22 @@
 (function() {
 	if (typeof jQuery == 'undefined') {
   	var e = document.createElement('script');
-		e.onload = function() {
+		
+		e.onload = function() { 
 			jQuery.noConflict();
+			
+			var l = 'Loaded jQuery' + jQuery.fn.jquery;
+			
+			if(typeof console == 'undefined') {
+				alert(l);
+			} else {
+				console.info(l);
+			}
+			
 		};
     
 		e.setAttribute('type', 'text/javascript');
     e.setAttribute('src', 'http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js');
 		document.body.appendChild(e);
-   }
+	}
 })();
